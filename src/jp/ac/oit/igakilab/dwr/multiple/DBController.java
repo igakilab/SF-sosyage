e30859c7a86484c8cea9a5052d35841d9d0a45e9
kjm2086 <2086psp@gmail.com>
2016-09-03 14:02:37 +0900

    MySQLへの対応変更
    MySQLを取り扱えるように変更しました

---
+++ src/jp/ac/oit/igakilab/dwr/multiple/DBController.java
@@ -0,0 +1,119 @@
+package jp.ac.oit.igakilab.dwr.multiple;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBController {
+	// スキーマ名、ユーザ名、パスワードはMySQLとの接続に必要なため
+	// ここで定数として定義しておく、使う時はここ変えてね
+	private static final String SCHEMA = "SFSDB";
+	private static final String USER = "root";
+	private static final String PASSWORD = "igakilabSF";
+
+	public void dowrite(String sql) {
+		// DBとのコネクション
+		Connection conn = null;
+		// DBの状態
+		Statement stmt = null;
+		// ファイルに触る関係上例外発生の可能性があるためcatchできるように
+		try {
+			// DBと接続するのに必要,詳しく知りたいなら"Class.forName"で検索(難しい話が山ほど出てくる)
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// DBとのコネクションを張る
+			conn = DriverManager
+					.getConnection("jdbc:mysql://localhost/" + SCHEMA + "?user=" + USER + "&password=" + PASSWORD);
+			// DBの状態を生成(？)
+			stmt = conn.createStatement();
+			// SQL文を実行し結果をrsに格納
+			stmt.executeUpdate(sql);
+			// SELECT id, text FROM message
+			// これはmessageテーブルからid,textをすべて読み込んで返すSQL
+		} catch (Exception e) {
+			// 例外処理(スタックトレースを表示)
+			e.printStackTrace();
+		} finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public String[] doget(String sql, int selectLength) {
+		// DBとのコネクション
+		Connection conn = null;
+		// DBの状態
+		Statement stmt = null;
+		// DBにSQL文を投げて戻ってきた結果
+		ResultSet rs = null;
+
+		List<String> list = new ArrayList<String>();
+		int elementNum = 0;
+
+		// ファイルに触る関係上例外発生の可能性があるためcatchできるように
+		try {
+			// DBと接続するのに必要,詳しく知りたいなら"Class.forName"で検索(難しい話が山ほど出てくる)
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// DBとのコネクションを張る
+			conn = DriverManager
+					.getConnection("jdbc:mysql://localhost/" + SCHEMA + "?user=" + USER + "&password=" + PASSWORD);
+			// DBの状態を生成(？)
+			stmt = conn.createStatement();
+			// SQL文を実行し結果をrsに格納
+			rs = stmt.executeQuery(sql);
+			// SELECT id, text FROM message
+			// これはmessageテーブルからid,textをすべて読み込んで返すSQL
+
+			// SQL文の結果が空になるまで1行ずつ読み込み
+			while (rs.next()) {
+				for (int i = 1; i <= selectLength; ++i) {
+					list.add(rs.getString(i));
+					elementNum = (i - 1) * selectLength;
+				}
+			}
+		} catch (Exception e) {
+			// 例外処理(スタックトレースを表示)
+			e.printStackTrace();
+		} finally {
+			// DBとの接続を開放
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// StringBuilderの中身をStringにして返却
+		return list.toArray(new String[elementNum]);
+	}
+}
--- src/jp/ac/oit/igakilab/dwr/multiple/TeamPrinter.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/TeamPrinter.java
@@ -7,11 +7,11 @@ public class TeamPrinter {
 	public List<String> fetchslot(LoginData user) throws InvalidValueException {
         List<String> list = new ArrayList<>();
         int userID = user.getuserID();
-        DBReader reader = new DBReader();
+        DBController reader = new DBController();
 
-        String[] result = reader.doget("SELECT slot1,slot2,slot3 FROM user WHERE userID == " + userID, 3);
+        String[] result = reader.doget("SELECT slot1,slot2,slot3 FROM user WHERE userID = " + userID, 3);
         for (int i = 0; i < 3; ++i) {
-        	list.add(reader.doget("SELECT imgaddr FROM character WHERE charaID == " + result[i], 1)[0]);
+        	list.add(reader.doget("SELECT imgaddr FROM sfchara WHERE charaID = " + result[i], 1)[0]);
         }
 
         return list;
@@ -21,12 +21,12 @@ public class TeamPrinter {
         int spot1 = user.getuserspot1();
         int spot2 = user.getuserspot2();
 
-        DBReader reader = new DBReader();
-        String[] slotch = reader.doget("SELECT slot"+ spot1 + ",slot" + spot2 +" FROM user WHERE userID == " + userID, 2);
+        DBController reader = new DBController();
+        String[] slotch = reader.doget("SELECT slot"+ spot1 + ",slot" + spot2 +" FROM user WHERE userID = " + userID, 2);
 
         reader.dowrite("UPDATE user SET "
         				+ "slot"+ spot1 + "=" + slotch[1] +
-        				", slot"+ spot2 + "=" + slotch[0] + " WHERE userID == " + userID);
+        				", slot"+ spot2 + "=" + slotch[0] + " WHERE userID = " + userID);
 
 
         return fetchslot(user);
