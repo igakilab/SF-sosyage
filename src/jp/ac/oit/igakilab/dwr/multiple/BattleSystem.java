9e217540961de8efbd056d3f33dc980781cf1f05
akimoto002 <akimoto.zemi002@gmail.com>
2016-09-09 14:57:07 +0900

    更新
    DBReaderからDBControllerに変更
    相手の体力をDBから読み取れるように変更
    自分の攻撃力と体力をDBから読み取れる(変更点:SQlite→mySQL)

--- src/jp/ac/oit/igakilab/dwr/multiple/BattleSystem.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/BattleSystem.java
@@ -7,12 +7,12 @@ public class BattleSystem {
 	public List<String> getatkhp(int userID) throws InvalidValueException {
 		List<String> list = new ArrayList<>();
 
-		DBReader reader = new DBReader();
-		int atk=0,hp=0;
+		DBController reader = new DBController();
+		int atk = 0, hp = 0;
 
-		String[] result = reader.doget("SELECT slot1,slot2,slot3 FROM user WHERE userID == " + userID, 3);
+		String[] result = reader.doget("SELECT slot1,slot2,slot3 FROM user WHERE userID = " + userID, 3);
 		for (int i = 0; i < 3; ++i) {
-			String[] tmp = reader.doget("SELECT atk,hp FROM character WHERE charaID == " + result[i], 2);
+			String[] tmp = reader.doget("SELECT atk,hp FROM sfchara WHERE charaID = " + result[i], 2);
 			atk += Integer.valueOf(tmp[0]);
 			hp += Integer.valueOf(tmp[1]);
 		}
@@ -20,6 +20,19 @@ public class BattleSystem {
 		list.add(String.valueOf(atk));
 		list.add(String.valueOf(hp));
 
+		return list;
+	}
+
+	public List<String> getenemy(int eneID) throws InvalidValueException {
+		List<String> list = new ArrayList<>();
+
+		DBController reader = new DBController();
+
+		String[] result = reader.doget("SELECT HP,AT,imageaddr FROM sfenechara WHERE eneID = " + eneID, 3);
+		list.add(String.valueOf(result[0]));
+		list.add(String.valueOf(result[1]));
+		list.add(String.valueOf(result[2]));
+		
 		return list;
 	}
 }
