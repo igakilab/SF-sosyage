abeea487f1fab320eb54b692c91c31be4ea95d58
kjm2086 <2086psp@gmail.com>
2016-09-29 18:16:16 +0900

    わからーん
    なぞ

--- src/jp/ac/oit/igakilab/dwr/multiple/LoginSystem.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/LoginSystem.java
@@ -5,12 +5,13 @@ public class LoginSystem {
 		String username = user.getusername();
 		String password = user.getpassword();
 		DBController reader = new DBController();
-		String[] result = reader.doget("SELECT password FROM user WHERE  username = " + username);
-		String[] ID = reader.doget("SELECT userID FROM user WHERE username = " + username);
-		if(result[0] == password){
+		String[] result = reader.doget("SELECT password FROM user WHERE username like '"+username+"'");
+		String[] ID = reader.doget("SELECT userID FROM user WHERE username LIKE '" + username + "'");
+
+		if(result[0].equals(password)){
 			return Integer.valueOf(ID[0]);
 		}
-		return 0;
+		return -1;
 
 	}
 	public void newuser(UserData user){
@@ -18,10 +19,7 @@ public class LoginSystem {
 		String password = user.getpassword();
 		DBController reader = new DBController();
 		String[] result = reader.doget("SELECT COUNT(userID) FROM user");
-		int usernumber = Integer.valueOf(result[0]);
-		reader.dowrite("INSERT INTO user (userID,username,password,slot1,slot2,slot3,chara1,chara2,chara3"
-				+ "chara4,chara5,chara6,chara7,chara8,chara9,chara10,chara11,chara12,chara13,turn,bestturn)VALUES(" +
-				usernumber +","+ username +","+password+",4,5,6,0,0,0,1,1,1,0,0,0,0,0,0,0,0,99999");
+		reader.dowrite("INSERT INTO user (userID,username,password,slot1,slot2,slot3,chara1,chara2,chara3,chara4,chara5,chara6,chara7,chara8,chara9,chara10,chara11,chara12,chara13,turn,bestturn)VALUES(" +result[0] +",'"+ username +"','"+password+"',4,5,6,0,0,0,1,1,1,0,0,0,0,0,0,0,0,99999)");
 	}
 
 }
