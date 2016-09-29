10990b5bfc8f316567c84cdd9946158bed0cb841
kjm2086 <2086psp@gmail.com>
2016-09-29 14:57:46 +0900

    とりあえず作りました
    なんか詰んでます

---
+++ src/jp/ac/oit/igakilab/dwr/multiple/UserData.java
@@ -0,0 +1,26 @@
+package jp.ac.oit.igakilab.dwr.multiple;
+
+public class UserData {
+	    private String username = "";
+	    private String password = "";
+
+	    public UserData(){
+	    }
+
+	    public void setusername(String username){
+	        this.username = username;
+	    }
+
+	    public String getusername(){
+	        return username;
+	    }
+
+		public String getpassword() {
+			return password;
+		}
+
+		public void setpassword(String password) {
+			this.password = password;
+		}
+}
+
