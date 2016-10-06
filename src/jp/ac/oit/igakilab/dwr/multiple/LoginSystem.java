93eee99b2fef3307a73909ba67a13d5df3253707
kjm2086 <2086psp@gmail.com>
2016-10-06 16:08:09 +0900

    iketa

--- src/jp/ac/oit/igakilab/dwr/multiple/LoginSystem.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/LoginSystem.java
@@ -1,26 +1,35 @@
 package jp.ac.oit.igakilab.dwr.multiple;
 
 public class LoginSystem {
-	public int login(UserData user){
+	public int login(UserData user) throws InvalidValueException{
 		String username = user.getusername();
 		String password = user.getpassword();
 		DBController reader = new DBController();
-		String[] result = reader.doget("SELECT password FROM user WHERE username like '"+username+"'");
+
 		String[] ID = reader.doget("SELECT userID FROM user WHERE username LIKE '" + username + "'");
+		if(ID.length == 0){
+			throw new InvalidValueException("IDもしくはパスワードが間違っています");
+		}
+		String[] result = reader.doget("SELECT password FROM user WHERE username like '"+username+"'");
 
 		if(result[0].equals(password)){
 			return Integer.valueOf(ID[0]);
+
 		}
-		return -1;
+		throw new InvalidValueException("IDもしくはパスワードが間違っています");
 
 	}
-	public void newuser(UserData user){
+	public void newuser(UserData user) throws InvalidValueException{
 		String username = user.getusername();
 		String password = user.getpassword();
 		DBController reader = new DBController();
-		String[] result = reader.doget("SELECT COUNT(userID) FROM user");
-		int x = Integer.valueOf(result[0]);
-		reader.dowrite("INSERT INTO user (userID,username,password,slot1,slot2,slot3,chara1,chara2,chara3,chara4,chara5,chara6,chara7,chara8,chara9,chara10,chara11,chara12,chara13,turn,bestturn)VALUES(6,'username','password',4,5,6,0,0,0,1,1,1,0,0,0,0,0,0,0,0,99999)");
+
+		String[] ID = reader.doget("SELECT userID FROM user WHERE username LIKE '" + username + "'");
+		if(ID.length >= 1){
+			throw new InvalidValueException("このIDは使用できません");
+		}
+		String[] result = reader.doget("SELECT COUNT(userID)+1 FROM user");
+		reader.dowrite("INSERT INTO user VALUES(" +  result[0] +",'" + username + "','"+password+"',4,5,6,0,0,0,1,1,1,0,0,0,0,0,0,0,0,99999)");
 	}
 
 }
