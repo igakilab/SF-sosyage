c425ab6ba87d3cc3b50c777061aa14de311ca5d4
kjm2086 <2086psp@gmail.com>
2016-10-26 20:45:35 +0900

    必殺の実装
    がんばる

--- src/jp/ac/oit/igakilab/dwr/multiple/Score.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/Score.java
@@ -1,12 +1,24 @@
 package jp.ac.oit.igakilab.dwr.multiple;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Score {
-	public  int scorecount (int turn,int userID){
+	public  List<String> scorecount (int turn,int userID){
 		DBController reader = new DBController();
-		String[] result = reader.doget("SELECT nextenemy FROM user WHERE userID = " + userID );
-		reader.dowrite("UPDATE user SET gachacount = " + turn +" WHERE userID = "+userID);
-		int x = Integer.valueOf(result[0]);
-		reader.dowrite("UPDATE user SET nextenemy = " + (x+1) +" WHERE userID = "+userID);
-		return turn;
+		List<String> list = new ArrayList<>();
+		String[] result = reader.doget("SELECT score FROM user WHERE userID = " + userID );
+		reader.dowrite("UPDATE user SET score = " +(Integer.valueOf(result[0])+ turn) +" WHERE userID = "+userID);
+		list.add(String.valueOf(turn));
+		result = reader.doget("SELECT gachacount FROM user WHERE userID = " + userID );
+		reader.dowrite("UPDATE user SET gachacount = " + (Integer.valueOf(result[0])+turn) +" WHERE userID = "+userID);
+		result = reader.doget("SELECT nextenemy FROM user WHERE userID = " + userID );
+		int x = Integer.valueOf(result[0])+1;
+		if(x>5){
+			list.add(String.valueOf(x));
+			return list;
+		}
+		reader.dowrite("UPDATE user SET nextenemy = " + x +" WHERE userID = "+userID);
+		return list;
 	}
 }
